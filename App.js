be081e087f42425c3d1271389da6616ea1cb440b
reigns20 <101939213+reigns20@users.noreply.github.com>
2025-03-09 00:15:46 +0800

    component
    add music player app

---
+++ App.js
@@ -0,0 +1,13 @@
+import Player from "./component/Player"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="music-card">
+        <Player />
+      </div>
+    </div>
+  );
+}
+
+export default App;
